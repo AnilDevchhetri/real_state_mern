bc394e42bb06e1875b33a2e7e02abdc317efcb75
AnilDevchhetri <chhetrivision@gmail.com>
2024-09-05 00:36:45 +0900

    Mongoconnection

--- api/index.js
+++ api/index.js
@@ -1,7 +1,23 @@
 import express from "express"
+import mongoose from "mongoose"
+import dotenv from "dotenv"
+
+dotenv.config();
+mongoose.connect(process.env.MONGO).then(()=>{
+   console.log("Connected to MongoDB") 
+}).catch((error)=>{
+    console.log(error)
+})
+
 
 const app = express();
 
 app.listen(3000, () =>{
     console.log('Server is runniogn on 3000')
 })
+
+//
+
+//mongo passswrod: cCKZ6lrib8Oe8aX7
+
+//mongodb+srv://realstate-mearn:cCKZ6lrib8Oe8aX7@realstate-mern.rrrsb.mongodb.net/mern-realstate?retryWrites=true&w=majority&appName=realstate-mern
