f437316ba892df609d5a47c6da23d424ba8f76bd
AnilDevchhetri <chhetrivision@gmail.com>
2024-09-11 01:37:54 +0900

    complete profiel page ui

--- api/index.js
+++ api/index.js
@@ -33,8 +33,7 @@ app.use((err ,req, res,next)=>{
 
 
 
-//3:25
-
+//3:42
 //mongo passswrod: cCKZ6lrib8Oe8aX7
 
 //mongodb+srv://realstate-mearn:cCKZ6lrib8Oe8aX7@realstate-mern.rrrsb.mongodb.net/mern-realstate?retryWrites=true&w=majority&appName=realstate-mern 
