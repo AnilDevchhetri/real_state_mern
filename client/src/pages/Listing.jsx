a428501a4958ac304f88ed7cf140c47e7a17e41a
AnilDevchhetri <chhetrivision@gmail.com>
2024-09-18 23:40:28 +0900

    8:4

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -1,56 +1,98 @@
-import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
-import {Swiper , SwiperSlide } from 'swiper/react'
-import  SwiperCore  from 'swiper'
-import {Navigation} from 'swiper/modules'
-import 'swiper/css/bundle';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore from "swiper";
+import { Navigation } from "swiper/modules";
+import "swiper/css/bundle";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBath, faBed, faChair, faLocation, faMap, faMapLocation, faParking } from '@fortawesome/free-solid-svg-icons'; // Import the bed icon
 const Listing = () => {
-    SwiperCore.use([Navigation]);
-    const [listing,setListing] = useState(null)
-    const [loading,setLoading] = useState(false);
-    const [error,setError]     = useState(false)
-    const params = useParams(); 
-    console.log(listing)
-    useEffect(()=>{
-        const fetchListing = async () =>{
-
-            try {
-                setLoading(true)
-                setError(false)
-                const listingId = params.listingId
-                console.log(listingId)
-                const res = await fetch(`/api/listing/get/${listingId}`);
-                const data =  await res.json();
-                if(data.success === false){setError(true);return;}
-                setListing(data);
-                setLoading(false)
-            } catch (error) {
-                setError(true)
-                setLoading(false)
-            }
-           
+  SwiperCore.use([Navigation]);
+  const [listing, setListing] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const params = useParams();
+  console.log(listing);
+  useEffect(() => {
+    const fetchListing = async () => {
+      try {
+        setLoading(true);
+        setError(false);
+        const listingId = params.listingId;
+        console.log(listingId);
+        const res = await fetch(`/api/listing/get/${listingId}`);
+        const data = await res.json();
+        if (data.success === false) {
+          setError(true);
+          return;
         }
-        fetchListing();
-    },[])
+        setListing(data);
+        setLoading(false);
+      } catch (error) {
+        setError(true);
+        setLoading(false);
+      }
+    };
+    fetchListing();
+  }, []);
   return (
     <main>
-        {loading && <p className='text-center my-7 text-2xl'>Loading...</p>}
-        {error && <p className='text-center my-7 text-2xl'>Somthing went wrong</p>}
-        {listing && listing.imageUrls && !loading && !error && (
-            <>
-                <Swiper navigation>
-                    {listing.imageUrls.map((url)=>(
-                        <SwiperSlide key={url}>
-                                <div className='h-[500px]' style={{background:`url(${url}) center no-repeat`, backgroundSize: 'cover'}}>
-
-                                </div>
-                        </SwiperSlide>
-                    ))}
-                </Swiper>
-            </>
-        )}
+      {loading && <p className="text-center my-7 text-2xl">Loading...</p>}
+      {error && (
+        <p className="text-center my-7 text-2xl">Somthing went wrong</p>
+      )}
+      {listing && listing.imageUrls && !loading && !error && (
+        <>
+          <Swiper navigation>
+            {listing.imageUrls.map((url) => (
+              <SwiperSlide key={url}>
+                <div
+                  className="h-[500px]"
+                  style={{
+                    background: `url(${url}) center no-repeat`,
+                    backgroundSize: "cover",
+                  }}
+                ></div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </>
+      )}
+      <div className="max-w-6xl mx-auto p-3">
+        <h2 className="text-black text-2xl font-semibold mt-3">
+          {listing.name}
+        </h2>
+        <p className="flex items-center w-full max-w-[200px] text-slate-600 text-center p-1 rounded-md">
+        <FontAwesomeIcon icon={faMapLocation}  className="text-lg pr-2 text-blue-700"/>
+        
+          {listing.address}
+        </p>
+        <div className="flex gap-4">
+          <p className="bg-blue-700 w-full max-w-[200px] text-white text-center p-1 rounded-md mt-3">
+            {listing.type === "rent" ? "For Rent" : "For Sale"}
+          </p>
+          {listing.offer && (
+            <p className="bg-gray-700 w-full max-w-[200px] text-white text-center p-1 rounded-md mt-3">
+              ${+listing.regularPrice - +listing.discountPrice}
+            </p>
+          )}
+        </div>
+        <p className="mt-3 text-slate-8 flex flex-col gap-2"><span className="font-semibold text-black text-2xl">About Property</span>
+            {listing.description}</p>
+        <ul className="mt-3  text-slate-900 text-sm sm:text-lg font-bold flex gap-4 sm:gap-6 items-center flex-wrap">
+            <li className="flex items-center gap-1 whitespace-nowrap"><FontAwesomeIcon icon={faBed}  className="text-lg"/>
+             {listing.bedrooms}{listing.bedrooms > 1 ?'beds':'bed'}</li>
+             <li className="flex items-center gap-1 whitespace-nowrap "><FontAwesomeIcon icon={faBath}  className="text-lg"/>
+             {listing.bathrooms}{listing.bathrooms > 1 ?'Baths':'Bath'}</li>
+             <li className="flex items-center gap-1 whitespace-nowrap "><FontAwesomeIcon icon={faParking}  className="text-lg"/>
+             {listing.bathrooms}{listing.bathrooms > 1 ?'Baths':'Bath'}</li>
+             <li className="flex items-center gap-1 whitespace-nowrap "><FontAwesomeIcon icon={faChair}  className="text-lg"/>
+             {listing.bathrooms}{listing.furnished > 1 ?'Fursnished':'Unfurnished'}</li>
+             
+        </ul>
+      </div>
     </main>
-  )
-}
+  );
+};
 
-export default Listing
+export default Listing;
