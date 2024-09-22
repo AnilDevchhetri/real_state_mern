4ab697eef5567a71ac8eca7c5bf93cda43ed3d38
AnilDevchhetri <chhetrivision@gmail.com>
2024-09-22 11:23:21 +0900

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className='py-20 p-3 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About Anil real Starte</h1>
+      <p className='mb-4 text-slate-700'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores, ex, non amet aperiam omnis dolorem hic beatae exercitationem deleniti quasi minus fugiat molestias, laboriosam repudiandae cupiditate. Sequi eaque ratione reprehenderit, adipisci aperiam molestias! Officiis ut doloribus veritatis nesciunt harum nulla.</p>
+      <p className='mb-4 text-slate-700'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facilis assumenda possimus eaque voluptates temporibus dicta repellat, quos ex quaerat recusandae?</p>
+    </div>
   )
 }
 
